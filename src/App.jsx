653762f0e648a6652ce1324db75cf386ab5c0ace
PheaSreynith <Sreynith.phea@student.cadt.edu.kh>
2026-02-26 16:00:53 +0700

    done step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import pnLogo from "./assets/pn-logo.png";
 import Header from "./Component/Header"
-
+import {HTML_RESULTS} from "./data.js"
+import Scores from "./Component/Scores.jsx"
 function App() {
   return (
     <>
@@ -10,31 +11,9 @@ function App() {
       </header>
 
       <main className="scores-container">
-        <div class="scores">
-          <h1>Fake Course</h1>
+        
+      <Scores courseName="HTML" courseResults={HTML_RESULTS} />
 
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
       </main>
     </>
   );
